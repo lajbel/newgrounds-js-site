444a1c3a8298a9b70e36cf0b959838a2b74cb6fe
lajbel <lajbelms@gmail.com>
2024-04-26 08:45:05 -0300

    add installation guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,25 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: "Newgrounds.js",
+            favicon: "./src/assets/favicon.ico",
+            logo: {
+                src: "./src/assets/newgrounds.js.png",
+                replacesTitle: true,
+            },
+            social: {
+                github: "https://github.com/lajbel/newgrounds-js",
+            },
+            sidebar: [
+                {
+                    label: "Installation",
+                    link: "/install/",
+                },
+            ],
+        }),
+    ],
 });
